8f846ce019d0cf91abde068884e53626b0c5fcd9
creyes25 <carolina.reyes2022@gmail.com>
2022-10-10 18:05:35 -0500

    Add bath component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Kitchen from './Kitchen'
 import LivingRoom from './LivingRoom'
+import Bath from './Bath'
 
 function App() {
   return (
     <div>
       <Kitchen />
       <LivingRoom />
+      <Bath size='Full'/>
+      <Bath size='Half'/>
+      
     </div>
   );
 }
