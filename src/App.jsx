883f0aca7727cf552dce0a8bc06b6494a3843177
creyes25 <carolina.reyes2022@gmail.com>
2022-10-10 18:09:34 -0500

    Add bedroom component

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import Kitchen from './Kitchen'
 import LivingRoom from './LivingRoom'
 import Bath from './Bath'
+import Bedroom from './Bedroom'
 
 function App() {
   return (
     <div>
+      <Bedroom bedNum='1'/>
       <Kitchen />
-      <LivingRoom />
       <Bath size='Full'/>
+      <Bedroom bedNum='2'/>
+      <LivingRoom />
       <Bath size='Half'/>
-      
+      <Bedroom bedNum='3'/>
+
     </div>
   );
 }
