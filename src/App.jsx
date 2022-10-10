37afaa6299b97d613bba8f1fbecf81bc9600a71d
creyes25 <carolina.reyes2022@gmail.com>
2022-10-10 17:55:16 -0500

    Add floor plan that includes kitchen and living room components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Kitchen from './Kitchen'
+import LivingRoom from './LivingRoom'
+
+function App() {
+  return (
+    <div>
+      <Kitchen />
+      <LivingRoom />
+    </div>
+  );
+}
+
+export default App;
