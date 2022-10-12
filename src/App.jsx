c293f308975fd77b5396f084ae353fb8f1f7b48f
creyes25 <carolina.reyes2022@gmail.com>
2022-10-11 21:08:07 -0500

    Add a floorplan component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import './App.css'
-import Kitchen from './Kitchen'
-import LivingRoom from './LivingRoom'
-import Bath from './Bath'
-import Bedroom from './Bedroom'
+import FloorPlan from './FloorPlan'
+
 
 function App() {
   return (
-    <div>
-      <Bedroom bedNum={1}/>
-      <Kitchen />
-      <Bath size='Full'/>
-      <Bedroom bedNum={2}/>
-      <LivingRoom />
-      <Bath size='Half'/>
-      <Bedroom bedNum={3}/>
-
-    </div>
+    <>
+      <FloorPlan />
+    </>
   );
 }
 
---
+++ src/FloorPlan.jsx
@@ -0,0 +1,21 @@
+import Kitchen from './Kitchen'
+import LivingRoom from './LivingRoom'
+import Bath from './Bath'
+import Bedroom from './Bedroom'
+
+
+const FloorPlan = () => {
+  return (
+    <div>
+      <Bedroom bedNum={1}/>
+      <Kitchen />
+      <Bath size='Full'/>
+      <Bedroom bedNum={2}/>
+      <LivingRoom />
+      <Bath size='Half'/>
+      <Bedroom bedNum={3}/>
+    </div>
+  );
+}
+
+export default FloorPlan;
